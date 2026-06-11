c3754b835847d93a7174564e04258ca6366d85aa
ducusin_j <ducusin_j@f112.ub.lan>
2026-06-11 09:56:34 +0800

    adding hoenn-gym-leaders-service

---
+++ src/app/hoenn-gym-leaders-service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { HoennGymLeadersService } from './hoenn-gym-leaders-service';
+
+describe('HoennGymLeadersService', () => {
+  let service: HoennGymLeadersService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(HoennGymLeadersService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
